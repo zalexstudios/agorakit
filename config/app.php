a95285233d564ef44992bb012f730229c477f29f
Philippe Jadin <philippe.jadin@gmail.com>
2020-05-06 18:33:59 +0200

    testing telescope

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
